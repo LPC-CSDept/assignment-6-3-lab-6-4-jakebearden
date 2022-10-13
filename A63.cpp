806ee537e9fad1846a135c2e1db418fdae07ac88
jake <jbearden364@gmail.com>
2022-10-12 19:34:33 -0700

    .

--- A63.cpp
+++ A63.cpp
@@ -19,16 +19,20 @@ int main()
 
 int getsum(int n)
 {
-    int amount, number, i,sum;
+    int amount, number, i,sum, currentmin, currentmax;
     cout << "enter number of inputs" << endl;
     cin >> n;
     cout << "Enter all " << n << " numbers" << endl;
     sum = 0;
+    currentmin = 10000;
+    currentmax = -10000;
     for (i = 0; i < n; i++)
     {
         cin >> number;
         sum +=number;
-
+        currentmin = min(currentmin,number);
+        currentmax = max(currentmax, number);
     }
+    sum = sum - currentmin - currentmax;
     return sum;
 }
